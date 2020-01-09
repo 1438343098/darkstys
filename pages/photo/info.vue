5f8159735556cf16b311d4ef0c8fe64959515f52
laoliu <1438343098@qq.com>
2020-01-09 11:19:15 +0800

    添加文件

---
+++ pages/photo/info.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="index">
+		详情
+	</view>
+</template>
+
+<script>
+	import {getPhotoInfo,getPhotoList} from "@/api/photo.js"
+	export default{
+		data(){
+			return{
+			}
+			
+		},
+		onLoad(){
+			this.getPhotoLists('图片')
+		},
+		methods:{
+		  async getPhotoLists(type,text=""){
+			  let data = (await getPhotoList({page:this.page,"text":text})).data
+			  if(type){	  
+				  this.newList = data.data.result
+				  this.list = this.list.concat(this.newList)
+				  return
+			  }
+			  this.list = data.data.result
+		  },
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
