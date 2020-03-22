23d14e54e165d5f1494185ba7cb2058d03b56df8
lhl <1438343098@qq.com>
2020-03-22 18:31:52 +0800

    修改接口

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	baseUrl:'http://192.168.232.129/api',//本地调试
+	// baseUrl:'http://192.168.232.129/api',//本地调试
 	// baseUrl:'',//h5
-	// baseUrl:'https://superliu.cn'//app 小程序
+	baseUrl:'https://superliu.cn/api'//app 小程序
 }
